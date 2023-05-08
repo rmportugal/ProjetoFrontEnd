784244427cda0255d1f2b39c1b87ef244f494e97
gabrielsn17 <gabriel.s.nogueira6@aluno.senai.br>
2023-05-08 16:05:48 -0300

    Projeto Final - Atualizado

--- Js/script.js
+++ Js/script.js
@@ -2,7 +2,7 @@ const slider = document.querySelector("#botao");
 const titulo = document.querySelector("#titulo");
 const paragrafo = document.querySelectorAll("p");
 const subtitulo = document.querySelectorAll("h2");
-const iconn = document.querySelector(".carousel-control-next-icon");
+const subtitulos = document.querySelectorAll("h5");
 
 slider.onclick = function (event) {
   if (event.target.checked){
@@ -10,11 +10,14 @@ slider.onclick = function (event) {
     titulo.style.color = "black";
     paragrafo.forEach(i => i.style.color = "black"); 
     subtitulo.forEach(i => i.style.color = "black");
+    subtitulos.forEach(i => i.style.color = "black");
+    
   }              
   else{
     document.body.style.background = "#2f3238"; 
     titulo.style.color = "white";
     paragrafo.forEach(i => i.style.color = "white"); 
-    subtitulo.forEach(i => i.style.color = "white");    
+    subtitulo.forEach(i => i.style.color = "white");
+    subtitulos.forEach(i => i.style.color = "white");    
   } 
 }

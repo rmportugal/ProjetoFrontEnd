4806fd03a04926f7e467e9b500b1fbf58611dbf4
gabrielsn17 <gabriel.s.nogueira6@aluno.senai.br>
2023-05-08 15:52:16 -0300

    Projeto Final - Completo

---
+++ ProjetoFinal/Js/script.js
@@ -0,0 +1,23 @@
+const slider = document.querySelector("#botao");
+const titulo = document.querySelector("#titulo");
+const paragrafo = document.querySelectorAll("p");
+const subtitulo = document.querySelectorAll("h2");
+const subtitulos = document.querySelectorAll("h5");
+
+slider.onclick = function (event) {
+  if (event.target.checked){
+    document.body.style.background = "white";
+    titulo.style.color = "black";
+    paragrafo.forEach(i => i.style.color = "black"); 
+    subtitulo.forEach(i => i.style.color = "black");
+    subtitulos.forEach(i => i.style.color = "black");
+    
+  }              
+  else{
+    document.body.style.background = "#2f3238"; 
+    titulo.style.color = "white";
+    paragrafo.forEach(i => i.style.color = "white"); 
+    subtitulo.forEach(i => i.style.color = "white");
+    subtitulos.forEach(i => i.style.color = "white");    
+  } 
+}
